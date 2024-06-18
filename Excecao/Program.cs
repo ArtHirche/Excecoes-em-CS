5d41e1764428d90457855cd66a288b737ac6734b
ArtHirche <hirche.arthur@gmail.com>
2024-06-18 12:42:05 -0300

    Exceções em C#

---
+++ Excecao/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        int valorUsuario = 0;
+        int valorConta = 4564;
+
+        try 
+        {
+            var totalPorPessoa = valorConta / valorUsuario;
+            Console.WriteLine(totalPorPessoa.ToString());
+        } catch (Exception ex) {
+            Console.WriteLine("Não foi possível executar a operação");
+        } finally
+        {
+            Console.WriteLine("Execução encerrou");
+        }
+        
+
+    }
+}
